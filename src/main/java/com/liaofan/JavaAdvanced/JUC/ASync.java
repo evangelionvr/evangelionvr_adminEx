099142a88c713622441fb45db9f40c9dd88097e4
廖钒 <liaofan@yinhai.com>
2022-05-12 09:18:36 +0800

    提交异步方法(使用多线程)

---
+++ src/main/java/com/liaofan/JavaAdvanced/JUC/ASync.java
@@ -0,0 +1,35 @@
+package com.liaofan.JavaAdvanced.JUC;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * 异步方法
+ */
+@Slf4j
+public class ASync {
+    public static void main(String[] args) {
+        new Thread(() -> {
+            FileReader fr = null;
+
+            try {
+                System.out.println("开始读取文件" + System.currentTimeMillis() + ",线程名:" + Thread.currentThread().getName());
+                fr = new FileReader("F:\\CloudMusic\\梁静茹 - 呵护.mp3");
+                char[] buf = new char[1024];
+                int len = 0;
+                while ((len = fr.read(buf)) != -1) {
+                    //System.out.println(new String(buf, 0, len));
+                }
+                System.out.println("读取mp3文件结束" + System.currentTimeMillis() + ",线程名:" + Thread.currentThread().getName());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }).start();
+        log.debug("main thread end" + ",线程名:" + Thread.currentThread().getName());
+        // 创建一个线程对象
+    }
+}
